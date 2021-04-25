cce920b89ba3d6495eb235e2f7fbf8de76a3471e
gregdavill <gregdavill@users.noreply.github.com>
2021-04-25 22:47:12 +0000

    deploy: ec2c9021e4c2d0d862ccd1d0e2e4b4a84f286c51

--- en.search.min.30db76b99fad29d946332393c8bd5055f65e8ad8afee0eb6d4cee0edb14da036.js
+++ en.search.min.30db76b99fad29d946332393c8bd5055f65e8ad8afee0eb6d4cee0edb14da036.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/OrangeCrab/flexsearch.min.js');loadScript('/OrangeCrab/en.search-data.min.5f5b79e0d075a999137661f5ae357f7c8a0e49bb7b9117baaa8a25b05cc55698.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/OrangeCrab/flexsearch.min.js');loadScript('/OrangeCrab/en.search-data.min.f90cc39204c5438054cc128650b0e0f9f46becd831ffc544fab206b0f9f1eed4.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
