2aac940c236964541f2740b7e13aa8f0ece16c1c
gregdavill <gregdavill@users.noreply.github.com>
2023-08-26 09:53:13 +0000

    deploy: 3df12aa40c695f6f986d12a8ab56ad1bcc3eab53

--- en.search.min.2140ce1a564ab66aea250497dc93ab52544371d17181123ae9f4d90cd0fb692d.js
+++
@@ -1,9 +0,0 @@
-'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');input.addEventListener('focus',init);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(e){if(input===document.activeElement){return;}
-const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
-input.focus();e.preventDefault();}
-function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/orangecrab-hardware/flexsearch.min.js');loadScript('/orangecrab-hardware/en.search-data.min.c361e5e57efbccea122d5d9e84c51ab2d2ba16d87da87108638d2a3c7e0bd65d.js',function(){input.required=false;search();});}
-function search(){while(results.firstChild){results.removeChild(results.firstChild);}
-if(!input.value){return;}
-const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
-function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
